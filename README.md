862808ce301705181f63f3210eca5b51c2d14c78
Ben-David <samuelbendavid01@gmail.com>
2023-12-04 06:11:34 +0100

    cleaning up

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# YouTube Downloader
+# YouTube Video Downloader using Pytube
 
-## 
+This Python script allows you to download individual YouTube videos or entire playlists using the Pytube library.
+
+## Prerequisites
+
+Make sure you have Python installed on your machine. You can install Pytube by running the following command:
+
+```bash
+pip install pytube
+```
+
+## Usage
+
+1. Clone this repository:
+
+```bash
+git clone https://github.com/your-username/youtube-downloader.git
+cd youtube-downloader
+```
+
+2. Run the script with the desired YouTube video URL or playlist URL:
+
+```bash
+python download_youtube.py
+```
+
+## Features
+
+Downloads individual YouTube videos with the highest resolution.
+Downloads entire playlists by iterating through each video.
--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,36 +1,84 @@
 from pytube import YouTube, Playlist
+from pytube.exceptions import PytubeError
+from http.client import IncompleteRead
 
-
+# Set the output path for the downloaded file
 DOWNLOAD_DIR = 'c:\\Users\\Chudi\\Downloads'
 
 print("YouTube Downloader")
 
 def single_video(link):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
     try:
-        youtubeObject.download(output_path=DOWNLOAD_DIR)
-    except:
-        print("An error has occurred")
-    print("Download is completed successfully")
+        max_retries = 3
+
+        # Create a YouTube object
+        yt = YouTube(link)
+
+        # Get the highest resolution stream
+        yt_stream = yt.streams.get_highest_resolution()
+
+        # Print video details
+        print(f"Video Title: {yt.title}")
+        print(f"Resolution: {yt_stream.resolution}")
+
+        # Get file size in bytes
+        file_size = yt_stream.filesize
+
+        # Convert bytes to megabytes for better readability
+        file_size_mb = file_size / (1024 * 1024)
+        print(f"File Size: {file_size_mb:.2f} MB")
+
+        # Download the video with retries
+        for attempt in range(1, max_retries + 1):
+            try:
+                # Download the video
+                print("Downloading... Please wait.")
+                yt_stream.download(output_path=DOWNLOAD_DIR)
+                print(f"Download complete! Saved at: {DOWNLOAD_DIR}")
+                break  # Exit the loop if download is successful
+            except IncompleteRead as e:
+                print(f"Retry {attempt}/{max_retries}: IncompleteRead error - {str(e)}")
+            except PytubeError as e:
+                print(f"PytubeError: {str(e)}")
+                break  # Exit the loop if there is an HTTP error that is not an IncompleteRead
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 def playlist(link):
-    ytPlayList = Playlist(link)
-    print(len(ytPlayList.video_urls))
-    for video in ytPlayList.videos:
-        try:
-            video.streams\
-                .filter(progressive=True, file_extension='mp4')\
-                    .order_by('resolution')\
-                        .desc()\
-                            .first().download(output_path=DOWNLOAD_DIR)
-        except:
-            print(f"An error has occurred, {ytPlayList.video_urls} does not exist")
-            print(f"Downloading... {len(ytPlayList.video_urls)}")
-    print("Playlist is completed successfully")
+    try:
+        max_retries = 3
 
-def Download():
-    video_type = input("Is it a single video or a playlist: ")
+        # Create a YouTube object
+        yt_playlist = Playlist(link)
+
+        # Print playlist details
+        print(f"Playlist Title: {yt_playlist.title}")
+        print(f"Number of Videos in Playlist: {len(yt_playlist.video_urls)}")
+
+        # Iterate through each video in the playlist
+        for video in yt_playlist.videos:
+            for attempt in range(1, max_retries + 1):
+                try:
+                    # Download the video with retries
+                    print("Downloading... Please wait.")
+                    video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(output_path=DOWNLOAD_DIR)
+                    print(f"Download of {video.title} complete!")
+                    break  # Exit the loop if download is successful
+                except IncompleteRead as e:
+                    print(f"Retry {attempt}/{max_retries}: IncompleteRead error - {str(e)}")
+                except PytubeError as e:
+                    print(f"PytubeError: {str(e)}")
+                    break  # Exit the loop if there is an HTTP error that is not an IncompleteRead
+
+        print("Download of the entire playlist is complete!")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+def main():
+    video_type = input("Is it a single video or a playlist \nType single for a single video and playlist: ")
 
     if video_type == "single":
         link = input("Enter the URL of the video to download: ")
@@ -41,5 +89,5 @@ def Download():
     else:
         print("Invalid option! Please choose either single or playlist.")
 
-
-Download()
+if __name__ == "__main__":
+    main()
